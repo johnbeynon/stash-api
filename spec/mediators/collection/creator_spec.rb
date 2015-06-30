3c54354edc29528992bbd6120e72b1650c5a44ef
John Beynon <john@beynon.org.uk>
2015-06-30 14:30:24 +0100

    add missing test

--- spec/mediators/collection/creator_spec.rb
+++ spec/mediators/collection/creator_spec.rb
@@ -1,5 +1,19 @@
 require "spec_helper"
 
 describe Mediators::Collection::Creator do
-  
+
+  let(:mediator) {described_class.new(@params)}
+
+  before do
+    @params = {
+      name: 'My Collection'
+    }
+  end
+
+  it 'creates a collection' do
+    collection = nil
+    expect{collection = mediator.call}.to change(Collection, :count).by(1)
+    expect(collection.name).to eq(@params[:name])
+  end
+
 end
